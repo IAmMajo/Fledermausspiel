c6cb4084a917b1f7755469af9a1860c9e8133691
31467 <nils.paassen@hsrw.org>
2022-12-18 23:06:56 +0100

    deactivated drag and gravity to replace it with own calculations. added break.

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -8,13 +8,19 @@ public class Bat : MonoBehaviour
     KeyCode flyingKey;
 
     [SerializeField]
+    KeyCode breakKey;
+
+    [SerializeField]
     float wingBeatsPerSecond;
 
     [SerializeField]
     float wingBeatVelocity;
 
     [SerializeField]
-    float rotationFactor;
+    float speedFactor;
+
+    [SerializeField]
+    float dragFactor;
 
     
 
@@ -35,7 +41,7 @@ public class Bat : MonoBehaviour
         {
             if (this.wingBeatDelayCounter == this.wingBeatDelay)
             {
-                comp_rb.AddForce(new Vector3(0,wingBeatVelocity,0)+transform.forward, ForceMode.VelocityChange);
+                comp_rb.AddForce(new Vector3(transform.forward.x,wingBeatVelocity,transform.forward.z), ForceMode.VelocityChange);
                 this.wingBeatDelayCounter = 0;
             }
             else
@@ -43,22 +49,35 @@ public class Bat : MonoBehaviour
                 this.wingBeatDelayCounter++;
             }
         }
+        else if(Input.GetKey(breakKey)) 
+        {
+            comp_rb.AddForce(comp_rb.velocity/3f*-1, ForceMode.Force);
+        }
+        else if(transform.eulerAngles.x>3 && transform.eulerAngles.x<21) //when you look down you should get faster/ values found trough log
+        {
+            comp_rb.AddForce(transform.forward*transform.eulerAngles.x*speedFactor, ForceMode.Acceleration);
+        }
         else
         {
+            //loss of velocity when you look up
+            comp_rb.AddForce(-1*(comp_rb.velocity*dragFactor)+transform.forward*dragFactor, ForceMode.Force);
             this.wingBeatDelayCounter = 0;
         }
         //places the transform to te transform of the camera parent(child with index 3) 
         if (Input.GetMouseButton(1))
         {
+            //as to be true otherwise to prevent random tumbeling
+            comp_rb.freezeRotation = false;
             //TO DO: make smooth
             transform.eulerAngles += transform.GetChild(3).eulerAngles-transform.eulerAngles;
         }
-
-        //when you look down you should get faster/ values found trough log
-        if(transform.eulerAngles.x>15 && transform.eulerAngles.x<21){
-            comp_rb.AddForce(transform.forward*transform.eulerAngles.x*5, ForceMode.Acceleration);
-            Debug.Log(true);
+        else
+        {
+            comp_rb.freezeRotation = true;
         }
+        
+
+        
             
         
     }
